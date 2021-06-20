93fd6cc28bfe1d94943ee2d32c7c1eaf8f49ddd9
Andreas Motl <andreas@hiveeyes.org>
2021-06-20 02:46:26 +0200

    Add project infrastructure

---
+++ tests/test_recorder.py
@@ -0,0 +1,20 @@
+import logging
+
+from gi.overrides import Gst
+
+from tests.util import load_module_from_file
+
+
+def test_spike():
+
+    spike = load_module_from_file("python/examples/flac-timestamp-chunked.py")
+
+    # Setup logging
+    spike.setup_logging(level=logging.DEBUG)
+
+    # Setup PyGObject and GStreamer
+    Gst.init(None)
+
+    # Run a basic pipeline test
+    pipe = spike.BasicPipeline()
+    pipe.setup()
---
+++ tests/util.py
@@ -0,0 +1,26 @@
+import imp
+
+try:
+    import hashlib
+    md = hashlib.md5
+except ImportError:
+    import md5
+    md = md5.new
+
+
+def load_module_from_file(path):
+    """
+    http://code.davidjanes.com/blog/2008/11/27/how-to-dynamically-load-python-code/
+
+    :param path:
+    :return:
+    """
+    try:
+        fp = open(path, 'rb')
+        digest = md(path.encode('utf-8')).hexdigest()
+        return imp.load_source(digest, path, fp)
+    finally:
+        try:
+            fp.close()
+        except:
+            pass
